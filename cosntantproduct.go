66d7cf3a787f943518c8e1ed2eae02f233aa6be1
GlendonK <coral456@>
2021-10-25 02:18:51 +0800

    asd

--- cosntantproduct.go
+++ cosntantproduct.go
@@ -7,30 +7,34 @@ import (
 func main() {
 	fmt.Println("Hello, playground")
 
-	wftm := 3260989
-	geist := 2362145
+	wftm := 3260989 // amt of token A
+	geist := 2362145 // amt of token B
+	
+	pwftm := 2.08 // price of token A
 
 	fmt.Printf("liquidity of wftm: %v\n", wftm)
 	fmt.Printf("liquidity of geist: %v\n", geist)
 
-	pgeist := float64(wftm) * 2.08
-	pgeist = pgeist / float64(geist)
+	pgeist := float64(wftm) * pwftm
+	pgeist = pgeist / float64(geist) // price of token B
 
-	k := wftm * geist
+	k := wftm * geist // constant product of the pool
 
 	fmt.Printf("k: %v\n", k)
 
 	fmt.Printf("price of geist: %f\n", pgeist)
+	
+	liqRemoved := 1900000
 
-	wftm = wftm - 1900000
+	wftm = wftm - liqRemoved // reduced token A liq
 
 	geist2 := k / wftm
 
 	fmt.Printf("liquidity of new wftm: %v\n", wftm)
 	fmt.Printf("liquidity of new geist: %v\n", geist2)
 
-	pgeist2 := float64(wftm) * 2.08
-	pgeist2 = pgeist2 / float64(geist2)
+	pgeist2 := float64(wftm) * pwftm 
+	pgeist2 = pgeist2 / float64(geist2) // new price of token B
 
 	fmt.Printf("price of new geist: %v\n", pgeist2)
 
